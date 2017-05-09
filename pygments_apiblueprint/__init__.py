86db307c549a8fac917bc0cb49d1f0cc0b37f783
Kyle Fuller <kyle@fuller.li>
2017-05-09 20:52:13 +0100

    feat: Add support for Sphinx auto loader

--- pygments_apiblueprint/__init__.py
+++ pygments_apiblueprint/__init__.py
@@ -1,1 +1,15 @@
 from pygments_apiblueprint.lexer import APIBlueprintLexer
+
+
+def setup(app):
+    """
+    Initializer for Sphinx extension API
+    http://www.sphinx-doc.org/en/stable/extdev/index.html#dev-extensions
+    """
+
+    lexer = APIBlueprintLexer()
+
+    add.add_lexer('apiblueprint', lexer)
+    add.add_lexer('apib', lexer)
+
+    return dict(version='0.1.0')
